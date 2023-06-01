cdfc54926d873ffe75f466499c25f13216081f88
Ian Cullen <ian.james.cullen@gmail.com>
2023-06-01 14:01:20 +0100

    Migrate main conanfile to v2
    Lots of standard processes have been refactored and wrapped in new
    sub-modules of the main conan module so make use of that functionality.

--- conanfile.py
+++ conanfile.py
@@ -1,26 +1,26 @@
 from conan import ConanFile
-from conan.tools.files import copy
 from conan.tools.apple import is_apple_os
-from conan.tools.cmake import CMakeToolchain, CMakeDeps, CMake, cmake_layout
+from conan.tools.scm import Git
+from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
 
 class QtColorWidgetsConan(ConanFile):
     name = "qt-color-widgets"
-    version = "conan_v2"
+    version = "9f4e052"
     license = "https://github.com/ess-dmsc/Qt-Color-Widgets/blob/master/COPYING"
     url = "https://github.com/ess-dmsc/Qt-Color-Widgets"
     description = "Improved QColorDialog and several other color-related widgets."
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False]}
     default_options = {"shared":False}
+    generators = "CMakeDeps"
 
     # The folder name when the *.tar.gz release is extracted
     folder_name = "Qt-Color-Widgets"
-    # The temporary build directory
-    build_dir = f"./{folder_name}/build"
 
     def source(self):
-        self.run("git clone https://github.com/ess-dmsc/Qt-Color-Widgets.git")
-        self.run(f"cd Qt-Color-Widgets && git checkout {self.version} && cd ..")
+        git = Git(self)
+        git.clone(url="https://github.com/ess-dmsc/Qt-Color-Widgets.git", target=".")
+        git.checkout(self.version)
 
     def layout(self):
         cmake_layout(self)
@@ -39,28 +39,11 @@ class QtColorWidgetsConan(ConanFile):
 
         tc.generate()
 
-        deps = CMakeDeps(self)
-        deps.generate()
-
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-
-        # mkdir(self, self.build_dir)
-        # with chdir(self, self.build_dir):
-        #
-        #     # cmake.configure(source_dir="..", build_dir=".")
-        #     self.run("cmake --debug-output %s %s" % ("..", cmake.command_line))
-        #     cmake.build(build_dir=".")
-        #     os.system("make install DESTDIR=./install")
-
-    def package(self):
-        copy(self, "*", self.source_folder, join(self.package_folder, "include"), keep_path=False)
-        copy(self, "*.dll", self.build_folder, join(self.package_folder, "bin"), keep_path=False)
-        copy(self, "*.so", self.build_folder, join(self.package_folder, "lib"), keep_path=False)
-        copy(self, "*.dylib", self.build_folder, join(self.package_folder, "lib"), keep_path=False)
-        copy(self, "*.a", self.build_folder, join(self.package_folder, "lib"), keep_path=False)
+        cmake.install()
 
     def package_info(self):
         self.cpp_info.libs = ["QtColorWidgets"]
